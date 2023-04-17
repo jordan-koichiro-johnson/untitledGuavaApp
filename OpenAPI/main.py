2d89afeab34c4c7c79597cf63cb58e4e0723eac2
Mia <113086524+MiaYun98@users.noreply.github.com>
2023-04-16 22:07:35 -0700

    apikey

--- OpenAPI/main.py
+++ OpenAPI/main.py
@@ -1,10 +1,10 @@
+import os
 import openai
-from api_secrets import API_KEY
 
-openai.api_key = API_KEY
-
-prompt = "say this is test"
-
-response = openai.Completion.create(engine="text-davinci-001", prompt=prompt, max_token = 6)
-
-print(response)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.Completion.create(
+  model="text-davinci-003",
+  prompt="Say this is a test",
+  max_tokens=7,
+  temperature=0
+)
